4aadfa9224c417277b94864dd5783449bba3af47
p4cm4n972 <manuel.adele@gmail.com>
2017-03-27 15:36:19 +0100

    modif

--- routes/index.js
+++ routes/index.js
@@ -9,9 +9,9 @@ var db = require('../public/javascripts/db.js');
 router.get('/', function (req, res, next) {
   var collection = db.get().collection('scores');
   collection.find().toArray(function (err, data) {
-    res.render('index', { title: 'Bienvenue sur JSUMOS',file: data, username: data[0].username, score: data[0].score});
+    res.render('index', { title: 'Bienvenue sur JSUMOS', file: data, username: data[0].username, score: data[0].score });
   });
-  });
+});
 
 
 router.post('/', function (req, res, next) {
@@ -19,21 +19,10 @@ router.post('/', function (req, res, next) {
   console.log(req.body.username);
   var collection = db.get().collection('player');
   collection.insert({ username: req.body.username });
-  res.render('log', { title: 'Bienvenue sur JSUMOS', file: data, player1: data[0], player2: data[1] });
+  res.render('index', { title: 'Bienvenue sur JSUMOS', file: data, player1: data[0], player2: data[1] });
 });
 
-router.get('/log', function (req, res, next) {
-  var collection = db.get().collection('player');
-  collection.find().toArray(function (err, play) {
-    player1 = play[1];
-    player2 = play[2];
-    var collection = db.get().collection('scores');
-    collection.find().toArray(function (err, data) {
-      console.log('READ DATABASE')
-      res.render('log', { title: 'Bienvenue sur JSUMOS', file: data, username: data[0].username, score: data[0].score, pseudo1: player1, pseudo2: player2 });
-    });
-  });
-});
+
 
 
 app.use('/', router);
--- routes/log.js
+++ routes/log.js
@@ -6,18 +6,18 @@ var app = express();
 
 var db = require('../public/javascripts/db.js');
 
-/*router.get('/', function (req, res, next) {
+router.get('/', function (req, res, next) {
   var collection = db.get().collection('player');
   collection.find().toArray(function (err, play) {
     player1 = play[1];
     player2 = play[2];
     var collection = db.get().collection('scores');
     collection.find().toArray(function (err, data) {
-      console.log('READ DATABASE')
+      console.log('READ DATABASE');
       res.render('log', { title: 'Bienvenue sur JSUMOS', file: data, username: data[0].username, score: data[0].score, pseudo1: player1, pseudo2: player2 });
     });
   });
-});*/
+});
 
 app.use('/', router);
 
